cea7840e5c796ab8dbe71badc7da9cb96a35126a
mesotokyo <meso@meso.tokyo>
2021-10-11 20:15:35 +0900

    v1.0.1: fix tiny bugs and texts
     * bugfix: change css's opacity unit to decimal from percentage
     * modification: unify text capitalization style
     * dev: add 'build' command to package.json

--- js/rail-pref-form.js
+++ js/rail-pref-form.js
@@ -6,14 +6,14 @@ export function railPrefForm(param, index) {
   });
   return `<div>
   <label class="form-item-inline">
-    <span class="label"> rail #${index + 1} type:</span>
+    <span class="label"> Rail #${index + 1} type:</span>
     <select class="w150" jumon-bind="type">
 ${options.join("\n      ")}
     </select>
   </label>
   <div>
     <label>
-      <input type="checkbox" jumon-bind="flip"> flip
+      <input type="checkbox" jumon-bind="flip"> Flip
     </label>
   </div>
 </div>
